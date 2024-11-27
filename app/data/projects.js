26c3f25617b05c0a9ca540031ad36ff6c9c31e19
NikileshReddyT <nikileshreddyt@gmail.com>
2024-11-28 00:29:03 +0530

    added new projects

--- app/data/projects.js
+++ app/data/projects.js
@@ -24,6 +24,22 @@ export const projects = [
     demoLink: 'https://klattcalculator.netlify.app/'
   },
   {
+    title: 'Sentiment Analysis',
+    description: 'A Sentiment Analysis System that analyzes text and provides insights into its sentiment. Features include text preprocessing, sentiment analysis, and real-time updates for users.',
+    image: '/projects/sentiment.png',
+    technologies: ['React.s', 'Gemini API','Nelify'],
+    githubLink: 'https://github.com/NikileshReddyT/Sentiment-Analysis-Gemini-API',
+    demoLink: 'https://sentimentanalizer.netlify.app/'
+  },
+  {
+    title: 'AI-Imgae-Generator',
+    description: 'An AI-powered image generation tool leveraging the Pollination API to create unique and diverse visual content. Users can input text prompts or parameters to generate custom images for various applications, from digital art to design prototypes.',
+    image: '/projects/aiimggen.png',
+    technologies: ['React.s', 'Pollination API','Nelify'],
+    githubLink: 'https://github.com/NikileshReddyT/AI_Image_Generation',
+    demoLink: 'https://fabulous-dodol-85d593.netlify.app/'
+  },
+  {
     title: 'Event Management System',
     description: 'A robust Event Management System that simplifies event planning and management. Features include event scheduling, attendee registration, and real-time updates for event organizers and attendees.',
     image: '/projects/event.png',
